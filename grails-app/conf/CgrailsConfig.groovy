a372d7fd7eaf5a14e5f53521d91463c882e559b3
manavmanocha <manav.manocha@comprotechnologies.com>
2012-10-06 16:12:05 +0530

    Updates for Cgrails enhancement 16

--- grails-app/conf/CgrailsConfig.groovy
+++ grails-app/conf/CgrailsConfig.groovy
@@ -1,13 +1,17 @@
 cgrails {
 	skinning {
 		baseskin = "default"
+		defaultskin = "default"
 		skins {
 			skin1 { parent = "default" }
 			skin2 { parent = "default" }
 		}		
 	}
+	workflows {
+		defaultwokflow = "singlepage"
+	}
 	less {
 		//Array of Less Files to compile.
 		files = ["styles"]
-	}
-}	
+	}    
+}
